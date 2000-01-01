579ea58fedca3c1ef0d38be91860db47b3f2b7fd
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Green repair and reproduction for 32bacc4a-764e-4c16-b9b6-4313e198ef8b-bug-2

--- internal/store/clone.go
+++ internal/store/clone.go
@@ -3,6 +3,7 @@ package store
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"trapreview/internal/domain"
 )
@@ -12,18 +13,40 @@ func cloneAggregate(source *domain.Aggregate) (*domain.Aggregate, error) {
 		return nil, fmt.Errorf("复制调查聚合: 源聚合为空")
 	}
 	target := *source
+	target.Survey.SpeciesCatalog = append([]string(nil), source.Survey.SpeciesCatalog...)
 	target.Stations = make(map[string]domain.CameraStation, len(source.Stations))
 	for id, station := range source.Stations {
+		station.RetrievedAt = cloneTimePointer(station.RetrievedAt)
 		target.Stations[id] = station
 	}
 	target.Observations = make(map[string]domain.Observation, len(source.Observations))
 	for id, observation := range source.Observations {
+		observation.QualityFlags = append([]string(nil), observation.QualityFlags...)
+		observation.LastVerifiedAt = cloneTimePointer(observation.LastVerifiedAt)
 		target.Observations[id] = observation
 	}
 	target.Adjudications = append([]domain.Adjudication(nil), source.Adjudications...)
-	target.Releases = append([]domain.DatasetRelease(nil), source.Releases...)
+	target.Releases = make([]domain.DatasetRelease, len(source.Releases))
+	for index, release := range source.Releases {
+		clonedCounts := make(map[string]int, len(release.SpeciesCounts))
+		for species, count := range release.SpeciesCounts {
+			clonedCounts[species] = count
+		}
+		release.SpeciesCounts = clonedCounts
+		release.Manifest.Items = append([]domain.ReleaseManifestItem(nil), release.Manifest.Items...)
+		target.Releases[index] = release
+	}
 	target.AuditTrail = append([]domain.AuditEntry(nil), source.AuditTrail...)
+	target.ApprovedAt = cloneTimePointer(source.ApprovedAt)
 	return &target, nil
+}
+
+func cloneTimePointer(value *time.Time) *time.Time {
+	if value == nil {
+		return nil
+	}
+	copy := value.UTC()
+	return &copy
 }
 
 func cloneRaw(source json.RawMessage) json.RawMessage { return append(json.RawMessage(nil), source...) }
